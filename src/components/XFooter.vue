cff1d334013685f6902e831f7fdcffc5d5d595f0
The X <haroune.official2@gmail.com>
2021-11-08 00:23:16 +0100

    add: empty components

---
+++ src/components/XFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "XFooter",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/XHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "XHeader",
+
+}
+</script>
+
+<style scoped>
+
+</style>
