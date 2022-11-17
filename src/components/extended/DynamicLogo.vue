a7e187ac75dcbc0887ce29b7ec2bfdd59d9d6988
The X <haroune.official2@gmail.com>
2022-11-17 21:36:28 +0100

    add: created the base dependencies, added the vue tailwind and vue kinesis and more

---
+++ src/components/extended/DynamicLogo.vue
@@ -0,0 +1,42 @@
+<script setup>
+import part1Logo from '@/assets/images/logo_part1.svg'
+import part2Logo from '@/assets/images/logo_part2.svg'
+import part3Logo from '@/assets/images/logo_part3.svg'
+</script>
+
+<template>
+    <div class="overflow-hidden">
+        <kinesis-container class="w-full h-full grid">
+            <kinesis-element 
+                tag="img"
+                :src="part3Logo"
+                :strength="-15"
+                type="depth"
+                transformOrigin="50% 300%"
+                axis="x"
+                class="x" />
+            <kinesis-element 
+                tag="img"
+                :src="part1Logo"
+                :strength="15"
+                type="depth"
+                transformOrigin="50% 300%"
+                axis="x"
+                class="x" />
+            <kinesis-element 
+                tag="img"
+                :src="part2Logo"
+                :strength="10"
+                type="depth"
+                transformOrigin="50% 300%"
+                class="x" />
+        </kinesis-container>
+        </div>
+</template>
+
+<style>
+.x{
+    grid-column: 1;
+    grid-row: 1;
+}
+</style>
