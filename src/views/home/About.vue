0c4dd4e3c4cd8b52d4e5b70c9b1b8a5155823673
The X <haroune.official2@gmail.com>
2022-11-24 21:01:25 +0100

    fix: added more to the description and fixed the spacing issues

--- src/views/home/About.vue
+++ src/views/home/About.vue
@@ -11,40 +11,48 @@ const sectionHover = ref(false)
     <section id="about" class="w-full h-screen flex flex-row items-center bg-dark"
         @mouseenter="sectionHover = true"
         @mouseleave="sectionHover = false">
+
         <div :class="['h-screen bg-primary transition-all select-none'
             , sectionHover? 'w-[15%]' : 'w-0 opacity-0']">
             <img src="../../assets/images/about_banner.svg" 
                 alt=""
                 class="w-full h-full object-contain">
         </div>
+
         <div :class="['h-screen transition-all grid items-center',
             'justify-center', sectionHover? 'w-[85%]' : 'w-full']">
 
             <div class="inline-flex flex-row items-center justify-between px-10">
                 <div class="h-full w-[50%] text-center cursor-pointer">
-                    <p class="text-primary font-bold text-4xl mb-7">WHO AM I?</p>
+                    <p class="text-primary font-bold text-4xl mb-10">WHO AM I?</p>
 
                     <p class="text-primary-light hover:text-primary font-semibold text-2xl 
-                        transition-all my-5">
+                        transition-all my-7">
                         Software engineer, with experience in software design, architecture and development.
                     </p>
 
                     <p class="text-primary-light hover:text-primary font-semibold text-2xl 
-                        transition-all my-5">
-                        Worked as an IT consultant.
+                        transition-all my-7">
+                        Worked as an IT consultant specialized in fullstack web apps building.
                     </p>
 
                     <p class="text-primary-light hover:text-primary font-semibold text-2xl 
-                        transition-all my-5">
-                        Native android apps developer & freelancer.
+                        transition-all my-7">
+                        Native android apps developer & freelancer for 4 years now.
                     </p>
 
                     <p class="text-primary-light hover:text-primary font-semibold text-2xl 
-                        transition-all my-5">
+                        transition-all my-7">
                         Part time front end developer & UI/UX designer.
                     </p>
 
+                    <p class="text-primary-light hover:text-primary font-semibold text-2xl 
+                        transition-all my-7">
+                        IoT and hardware communication enthusiast.
+                    </p>
+
                 </div>
+
                 <div class="w-[50%] grid">
                     <div class="w-[70%] m-auto grid">
                         <img src="../../assets/images/logo_bg.png" 
@@ -56,10 +64,10 @@ const sectionHover = ref(false)
                                 alt=""
                                 class="w-[60%]">
                         </div>
-                        
                     </div>
                 </div>
             </div>
+
         </div>
     </section>
 </template>
