830a27d355efaaef1188b2b9615d734984fbddf2
The X <haroune.official2@gmail.com>
2022-11-26 00:37:07 +0100

    finish: the last project section

--- src/components/base/Project.vue
+++ src/components/base/Project.vue
@@ -3,6 +3,8 @@ import Github from '../../assets/icons/github.vue';
 import Link from '../../assets/icons/link.vue';
 import AnimatedLogo from '../extended/AnimatedLogo.vue';
 
+const props = defineProps(['title','description', 'git', 'link', 'stack'])
+
 const redirect = (link)=>{
     window.open(link, "_blank")
 }
@@ -19,35 +21,29 @@ const redirect = (link)=>{
                 <AnimatedLogo class="w-[40px] h-[40px]"/>
 
                 <div class="inline-flex flex-row items-center gap-x-3 mx-2">
-                    <Github class="w-5 h-5 fill-white hover:fill-primary transition-all" 
-                        @click="redirect('https://google.com')"/>
+                    <Github 
+                        v-if="props.git.length > 0"
+                        class="w-5 h-5 fill-white hover:fill-primary transition-all" 
+                        @click="redirect(props.git)"/>
                         
-                    <Link class="w-5 h-5 fill-white hover:fill-primary transition-all"
-                        @click="redirect('https://google.com')"/>
+                    <Link 
+                        v-if="props.link.length > 0"
+                        class="w-5 h-5 fill-white hover:fill-primary transition-all"
+                        @click="redirect(props.link)"/>
                 </div>
             </div>
 
-            <p class="font-semibold text-primary text-lg my-2">Project name</p>
-            <p class="font-normal text-white text-sm mb-2">This will be a description for each project that is listed in this grid.</p>
+            <p class="font-semibold text-primary text-lg my-2">{{props.title}}</p>
+            <p class="font-normal text-white text-sm mb-2">{{props.description}}</p>
 
             <div>
-                <div class="inline-flex flex-row items-center mx-1">
+                <div
+                    v-for="tech in props.stack"
+                    class="inline-flex flex-row items-center mx-1">
                     <img src="../../assets/icons/ic_sun.svg" 
                         alt=""
                         class="h-3 w-3 mr-1">
-                    <p class="inline text-primary text-xs font-medium">Kotlin</p>
-                </div>
-                <div class="inline-flex flex-row items-center mx-1">
-                    <img src="../../assets/icons/ic_sun.svg" 
-                        alt=""
-                        class="h-3 w-3 mr-1">
-                    <p class="inline text-primary text-xs font-medium">Android</p>
-                </div>
-                <div class="inline-flex flex-row items-center mx-1">
-                    <img src="../../assets/icons/ic_sun.svg" 
-                        alt=""
-                        class="h-3 w-3 mr-1">
-                    <p class="inline text-primary text-xs font-medium">Jetpack compose</p>
+                    <p class="inline text-primary text-xs font-medium">{{tech}}</p>
                 </div>
             </div>
         </div>
--- src/views/home/Projects.vue
+++ src/views/home/Projects.vue
@@ -3,13 +3,70 @@ import { ref } from 'vue';
 import AnimatedLogo from '@/components/extended/AnimatedLogo.vue';
 import Project from '../../components/base/Project.vue';
 
-
 const sectionHover = ref(true)
+
+const projects = ref([
+    {
+        name: 'Milky store backend',
+        description: 'The backend developed for milky store mobile application.',
+        stack: ['Kotlin', 'Ktor', 'PgSql'],
+        github: '',
+        link: ''
+    },
+    {
+        name: 'Strawberry market',
+        description: 'A first concept version of the mobile application "Milky store".',
+        stack: ['Kotlin', 'Xml'],
+        github: '',
+        link: ''
+    },
+    {
+        name: 'Emerald system',
+        description: 'A full design system the bridges the gap between the mobile and the web.',
+        stack: ['Adobe XD'],
+        github: '',
+        link: ''
+    },
+    {
+        name: 'Emerald react',
+        description: 'A component library made for react 17, based on the emerald design system.',
+        stack: ['React', 'Tailwind', 'Storybook'],
+        github: '',
+        link: ''
+    },
+    {
+        name: 'Emerald live',
+        description: 'A live website that demonstrate the emerald design system components.',
+        stack: ['Kotlin', 'Ktor', 'PgSql'],
+        github: '',
+        link: ''
+    },
+    {
+        name: 'Aaron branding',
+        description: 'The official branding of my own personal brand "Aaron nerox".',
+        stack: ['Affinity designer'],
+        github: '',
+        link: ''
+    },
+    {
+        name: 'CSE rebrand',
+        description: 'A full rebranding of the visual identity of one of the oldest student clubs in algeria, CSE.',
+        stack: ['Adobe illustrator'],
+        github: '',
+        link: ''
+    },
+    {
+        name: 'The X dev brand',
+        description: 'A full brand identity design for the dev brand named "The X".',
+        stack: ['Affinity designer'],
+        github: '',
+        link: ''
+    },
+])
 </script>
 
 <template>
-    <section id="about" class="w-full h-screen flex flex-row items-center"
-        >
+    <section id="about" class="w-full h-screen flex flex-row items-center">
 
         <div :class="['h-screen bg-primary transition-all select-none'
             , sectionHover? 'w-[15%]' : 'w-0 opacity-0']">
@@ -26,17 +83,13 @@ const sectionHover = ref(true)
 
                 <div class="w-fit h-fit overflow-x-auto grid grid-cols-5 p-3 gap-8">
                    
-                        <Project />
-                        <Project />
-                        <Project />
-                        <Project />
-                        <Project />
-                        <Project />
-                        <Project />
-                        <Project />
-                        <Project />
-                        <Project />
-                  
+                        <Project 
+                            v-for="project in projects"
+                            :title="project.name"
+                            :description="project.description"
+                            :git="project.github"
+                            :link="project.link"
+                            :stack="project.stack" />
                 </div>
             </div>
 
