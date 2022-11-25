38dca5962108a805cfbcae373edc5d8f36c69df5
The X <haroune.official2@gmail.com>
2022-11-26 00:49:00 +0100

    finish: adding the links to the rest of the projects

--- src/views/home/Projects.vue
+++ src/views/home/Projects.vue
@@ -10,14 +10,14 @@ const projects = ref([
         name: 'Milky store backend',
         description: 'The backend developed for milky store mobile application.',
         stack: ['Kotlin', 'Ktor', 'PgSql'],
-        github: '',
+        github: 'https://github.com/TheX-ByAaron/MilkyStoreBackend',
         link: ''
     },
     {
         name: 'Strawberry market',
         description: 'A first concept version of the mobile application "Milky store".',
         stack: ['Kotlin', 'Xml'],
-        github: '',
+        github: 'https://github.com/TheX-ByAaron/TheStrawberryMarket',
         link: ''
     },
     {
@@ -25,20 +25,20 @@ const projects = ref([
         description: 'A full design system the bridges the gap between the mobile and the web.',
         stack: ['Adobe XD'],
         github: '',
-        link: ''
+        link: 'https://www.behance.net/gallery/135931393/Emerald-design-system'
     },
     {
         name: 'Emerald react',
         description: 'A component library made for react 17, based on the emerald design system.',
         stack: ['React', 'Tailwind', 'Storybook'],
-        github: '',
-        link: ''
+        github: 'https://github.com/TheX-ByAaron/Emerald-react-lib',
+        link: 'https://www.npmjs.com/package/emerald-react'
     },
     {
         name: 'Emerald live',
         description: 'A live website that demonstrate the emerald design system components.',
-        stack: ['Kotlin', 'Ktor', 'PgSql'],
-        github: '',
+        stack: ['Vuejs'],
+        github: 'https://github.com/TheX-ByAaron/Emerald_live',
         link: ''
     },
     {
@@ -46,21 +46,21 @@ const projects = ref([
         description: 'The official branding of my own personal brand "Aaron nerox".',
         stack: ['Affinity designer'],
         github: '',
-        link: ''
+        link: 'https://www.behance.net/gallery/133566979/Aaron-nerox-personal-brand-identity'
     },
     {
         name: 'CSE rebrand',
         description: 'A full rebranding of the visual identity of one of the oldest student clubs in algeria, CSE.',
         stack: ['Adobe illustrator'],
         github: '',
-        link: ''
+        link: 'https://www.behance.net/gallery/133863929/CSE-Brand-identity-refresh'
     },
     {
         name: 'The X dev brand',
         description: 'A full brand identity design for the dev brand named "The X".',
         stack: ['Affinity designer'],
         github: '',
-        link: ''
+        link: 'https://www.behance.net/gallery/133711717/The-X-Developer-studio-brand-design'
     },
 ])
 </script>
