068d10dc02a4bf9eda3ccb585b2166a1f5fd6d70
aaron-nerox <haroune.official2@gmail.com>
2023-12-05 13:18:48 +0400

    finish: finished dynamic button and responsive splash screen design

--- src/views/misc/Splash.vue
+++ src/views/misc/Splash.vue
@@ -2,36 +2,27 @@
 import { watchEffect } from "vue";
 import LottieAnimation from "../../components/extended/LottieAnimation.vue"
 import infinityAnimation from "@/assets/animations/infinity_animation.json"
-import { useRouter } from "vue-router";
+import { useRouter } from "vue-router"
+import Button from '../../components/base/Button.vue'
 
 
 let router = useRouter()
 
-watchEffect(() => {
-    setTimeout(() => {
-        router.replace('/home')
-    }, 3000)
-})
+// watchEffect(() => {
+//     setTimeout(() => {
+//         router.replace('/home')
+//     }, 3000)
+// })
 </script>
 
 
 <template>
     <div class="w-full h-screen inline-flex items-center justify-center bg-light">
-        <img 
-            src="@/assets/images/background_overlay_bottom_start.svg" 
-            alt="background element"
-            class="w-1/4 absolute bottom-0 left-0"
-        />
-        <img 
-            src="@/assets/images/background_overlay_top_end.svg" 
-            alt="background element"
-            class="w-1/4 absolute top-0 right-0"
-        />
-        <LottieAnimation 
-            :animationData="infinityAnimation"
-            :isLooping="true"
-            :autoPlay="true"
-            class="w-[360px] h-[360px]"
-        />
+        <img src="@/assets/images/background_overlay_bottom_start.svg" alt="background element"
+            class="md:w-1/4 w-1/2 absolute bottom-0 left-0" />
+        <img src="@/assets/images/background_overlay_top_end.svg" alt="background element"
+            class="md:w-1/4 w-1/2 absolute top-0 right-0" />
+        <LottieAnimation :animationData="infinityAnimation" :isLooping="true" :autoPlay="true"
+            class="md:w-[420px] w-[256px] h-[420px]" />
     </div>
 </template>
