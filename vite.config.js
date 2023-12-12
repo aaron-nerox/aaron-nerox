03548576c348cf4e39d2b80340336f234f722a54
aaron-nerox <haroune.official2@gmail.com>
2023-12-12 17:12:29 +0400

    fix: fixed vite modules referencing

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
         manualChunks: {
           'group-home': [
             './src/views/Home.vue',
-            './src/views/NotFound.vue',
-            './src/views/NeowallsPolicy.vue',
+            './src/views/misc/NotFound.vue',
+            './src/views/misc/NeowallsPolicy.vue',
             './src/views/misc/Splash.vue',
           ],
         },
