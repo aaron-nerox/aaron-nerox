b260f41ddc3629e7a555284c5bb4cf3d3f100ead
aaron-nerox <haroune.official2@gmail.com>
2023-12-26 18:07:03 +0400

    fix: fixed broken interation with the menu layer

--- src/components/navigation/Menu.vue
+++ src/components/navigation/Menu.vue
@@ -12,10 +12,13 @@ defineEmits(['dissmiss'])
 <template>
     <transition name="fade">
         <div v-if="props.open" 
+            
             class="min-w-full min-h-screen w-full h-screen fixed top-0 
                 bottom-0 right-0 left-0 bg-primary/30 backdrop-blur-md 
                 z-30 select-none grid">
-            <div class="w-full h-full py-10 m-auto flex flex-col items-center justify-between">
+            <div 
+                @click.self="$emit('dissmiss')"
+                class="w-full h-full py-10 m-auto flex flex-col items-center justify-between">
                 <div></div>
                 <div class="w-2/3 md:w-1/4 flex flex-col items-center gap-y-10">
                     <a href="#about" class="!w-full" @click="$emit('dissmiss')">
@@ -50,7 +53,7 @@ defineEmits(['dissmiss'])
                         </Button>
                     </a>
                 </div>
-                <p class="text-light text-sm">
+                <p class="text-dark text-sm">
                     Where there is light, you will find darkness ☯.
                 </p>
             </div>
